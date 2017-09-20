1e2af556f19560d230fca24307122fa48fe9957a
Daniel Bucket <danieljbucket@gmail.com>
2017-09-20 09:11:06 -0600

    DetailsPage component test

---
+++ react-ui/src/tests/componentTests/DetailPageReducer.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { shallow, mount } from 'enzyme';
+
+import DetailsPage from '../components/DetailsPage';
+
+describe('DetailsPageReducer', () => {
+  it.only('should ', () => {
+    const wrapper = shallow(DetailsPage);
+    console.log(wrapper);
+  });
+});
